ce89b2f9b1b068017790b40b22518ee7318aa049
Chetan Godhani <chetangodhani9@gmail.com>
2019-04-09 21:54:29 +0530

    CSS Modules for Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from "react";
-import './Person.css';
+import classes from './Person.module.css';
 
 const person = (props) => {
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p>I am {props.name}, and {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
